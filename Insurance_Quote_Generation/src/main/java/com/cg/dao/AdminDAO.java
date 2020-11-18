d54dcc621cd4db2924a43a3be6560ef56aeeb817
Acer <Acer@192.168.0.5>
2020-11-18 13:27:03 +0530

    second commit

--- Insurance_Quote_Generation/src/main/java/com/cg/dao/AdminDAO.java
+++ Insurance_Quote_Generation/src/main/java/com/cg/dao/AdminDAO.java
@@ -9,6 +9,9 @@ package com.cg.dao;
 	import java.util.List;
 
 	import com.cg.Exception.QGSException;
+import com.cg.model.Accounts;
+import com.cg.model.Policy;
+import com.cg.model.UserRole;
 	
 
 	public class AdminDAO implements IAdminDAO{
@@ -21,7 +24,139 @@ package com.cg.dao;
 		
 		
 		public boolean loginValidation(String username, String password) throws QGSException {
+			return false;
 		
+		}
+
+
+
+
+		public String getRoleCode(String userName, String password) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public int addUser(UserRole userRole) throws QGSException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+
+
+
+		public String getLineOfBusinessIdByName(String lineOfBusinessName) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public boolean isUserExists(String userName) throws QGSException {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+
+
+
+		public int accountCreation(Accounts account, String userName) throws QGSException {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+
+
+
+		public String getBusSegId(int accNumber) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public com.sun.xml.internal.bind.v2.schemagen.xmlschema.List getPolicyQuestions(String busSegId)
+				throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public Integer createPolicy(Policy policy) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public Integer getPolicyNumber() throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public void addPolicyDetails(int polNumber, com.sun.xml.internal.bind.v2.schemagen.xmlschema.List questionIds,
+				com.sun.xml.internal.bind.v2.schemagen.xmlschema.List selectedAnswers) throws QGSException {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+
+
+		public com.sun.xml.internal.bind.v2.schemagen.xmlschema.List getPolicies() throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public Accounts getAccountDetails(Integer accNumber) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public String getBusSegName(String busSegId) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public Double getPolicyPremium(Integer polNum) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public com.sun.xml.internal.bind.v2.schemagen.xmlschema.List getSelectedAnswers(int polNum)
+				throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+		public Integer getPolicyPremiumAmount(Integer sumOfWeightages) throws QGSException {
+			// TODO Auto-generated method stub
+			return null;
 		}
 	}
 
--- Insurance_Quote_Generation/src/main/java/com/cg/dao/IAdminDAO.java
+++ Insurance_Quote_Generation/src/main/java/com/cg/dao/IAdminDAO.java
@@ -1,5 +1,50 @@
 package com.cg.dao;
 
-public interface IAdminDAO {
+import com.cg.Exception.QGSException;
+import com.cg.model.Accounts;
+import com.cg.model.Policy;
+import com.cg.model.UserRole;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import java.util.AbstractList;
+
+public interface IAdminDAO<PolicyQuestions> {
+	
+public boolean loginValidation(String userName, String password) throws QGSException;
+	
+	public String getRoleCode(String userName, String password) throws QGSException;
+	
+	public int addUser(UserRole userRole) throws QGSException;
+	
+	public String getLineOfBusinessIdByName(String lineOfBusinessName) throws QGSException;
+
+	public boolean isUserExists(String userName) throws QGSException;
+	
+	public int accountCreation(Accounts account, String userName) throws QGSException;
+
+	
+	public String getBusSegId(int accNumber) throws QGSException;
+	
+	public List getPolicyQuestions(String busSegId) throws QGSException;
+	
+	public Integer createPolicy(Policy policy) throws QGSException;
+	
+	public Integer getPolicyNumber() throws QGSException;
+	
+	public void addPolicyDetails(int polNumber, List questionIds, List selectedAnswers) throws QGSException;
+	
+	public List getPolicies() throws QGSException;
+	
+	public Accounts getAccountDetails(Integer accNumber) throws QGSException;
+	
+	public String getBusSegName(String busSegId) throws QGSException;
+
+	public Double getPolicyPremium(Integer polNum) throws QGSException;
+
+	public List getSelectedAnswers(int polNum) throws QGSException;
+
+	public Integer getPolicyPremiumAmount(Integer sumOfWeightages) throws QGSException;
 
 }
+
+
+
