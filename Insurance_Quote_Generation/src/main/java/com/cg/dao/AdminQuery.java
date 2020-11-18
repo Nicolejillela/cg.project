ca553da3a149757e2618f9f3c7a2d82cde2f1203
Acer <Acer@192.168.0.5>
2020-11-18 14:07:28 +0530

    second commit

---
+++ Insurance_Quote_Generation/src/main/java/com/cg/dao/AdminQuery.java
@@ -0,0 +1,20 @@
+package com.cg.dao;
+
+public class AdminQuery {
+
+String VALIDATE_USER_QUERY = "select * from UserRole where UserName = ? and Password = ?";
+	
+	String GET_ROLECODE = "select RoleCode from UserRole where UserName = ? and Password = ?";
+	
+	String ADD_USER = "insert into UserRole values(?,?,?)";
+	
+	String ACCOUNT_CREATION = "insert into Accounts values(Account_Number.nextval,?,?,?,?,?,?,?)";
+	
+	String GET_LOB_NAME = "select Bus_Seg_Id from BusinessSegment where Bus_Seg_Name = ?";
+	
+	String USER_EXISTS = "select * from UserRole where username = ?";
+	
+	
+	
+}
+
