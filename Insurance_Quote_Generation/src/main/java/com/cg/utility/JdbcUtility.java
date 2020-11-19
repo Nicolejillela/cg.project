a806cffc6559158315ce2ea6065380175c109502
Acer <Acer@192.168.0.6>
2020-11-19 11:49:03 +0530

    Third commit

---
+++ Insurance_Quote_Generation/src/main/java/com/cg/utility/JdbcUtility.java
@@ -0,0 +1,35 @@
+package com.cg.utility;
+
+
+
+	
+	import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.cg.Exception.QGSException;
+
+import oracle.jdbc.OracleDriver;
+
+//This class is used to get the connection object
+public class JdbcUtility {
+	
+	private static Connection connection = null;
+
+	static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static Connection getConnection() throws QGSException {
+		
+		try {
+			DriverManager.registerDriver(new OracleDriver());
+			connection = DriverManager.getConnection(url, "system", "Capgemini123");
+			System.out.println("connected");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new QGSException("Error occurred in connecting to database"+e.getMessage());
+		}
+		return connection;
+	}
+}
+
+
